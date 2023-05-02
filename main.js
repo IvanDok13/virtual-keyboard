6aeb29748b32ae36705f9261a20cdaa1b2875b73
Ivan Klochkov <Klochkov.Ivan.Sergeevich@gmail.com>
2023-05-02 15:12:50 +0300

    feat: update style.css and main.js change columns shape

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 const keyboard = [
-    // 192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 8,
-    // 9, 81,
-
     113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 92, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47];
 
 // document.onkeypress = function (event) {
@@ -28,9 +25,9 @@ keys.className = 'keys';
 let initKey = () => {
     let out = '';
     for (let i = 0; i < keyboard.length; i++) {
-        if (i === 13 || i == 27 || i === 40 || i === 52 || i === 64) {
-            out += '<div class="row"></div>'
-        }
+        // if (i === 12 || i == 27 || i === 40 || i === 52 || i === 64) {
+        //     out += '<div class="row"></div>'
+        // }
         out += '<div id="key-char-code" class="key-char-code" data="' + keyboard[i] + '">' + String.fromCharCode(keyboard[i]) + '</div>';
     }
     keys.innerHTML = out;
